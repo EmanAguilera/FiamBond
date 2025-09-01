300da196a9931b96f55e9a76069384bcdd060a76
Eman <emanaguilera711@gmail.com>
2025-08-31 19:40:10 -0700

    Add or update functionality of main.jsx

--- my_cointrak_react/src/main.jsx
+++ my_cointrak_react/src/main.jsx
@@ -1,11 +1,19 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import AppProvider from "./Context/AppContext.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import AppProvider from './Context/AppContext.jsx';
+import './App.css'; // Assuming you have this for global styles
 
-ReactDOM.createRoot(document.getElementById("root")).
-render(
-<AppProvider>
-<App />
-</AppProvider>
+// 1. Import BrowserRouter from react-router-dom
+import { BrowserRouter } from 'react-router-dom';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. Wrap your entire application with BrowserRouter */}
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
