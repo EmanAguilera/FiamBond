3849055ca55bddfdc6fc40c49dbee8e50570716b
Eman <emanaguilera711@gmail.com>
2025-09-19 07:35:34 -0700

    Design: All of pagination of app

--- my_cointrak_react/src/Pages/Families/Ledger.jsx
+++ my_cointrak_react/src/Pages/Families/Ledger.jsx
@@ -124,13 +124,31 @@ export default function FamilyLedger() {
               )) : <p className="p-4">No transactions for this family in this period.</p>}
             </div>
 
+            {/* --- START: UPDATED PAGINATION CONTROLS --- */}
             {pagination && pagination.last_page > 1 && (
               <div className="flex justify-between items-center mt-6">
-                <button onClick={() => getReport(pagination.current_page - 1)} disabled={pagination.current_page === 1} className="secondary-btn disabled:opacity-50">&larr; Previous</button>
-                <span className="text-sm text-gray-600">Page {pagination.current_page} of {pagination.last_page}</span>
-                <button onClick={() => getReport(pagination.current_page + 1)} disabled={pagination.current_page === pagination.last_page} className="secondary-btn disabled:opacity-50">Next &rarr;</button>
+                <button
+                  onClick={() => getReport(pagination.current_page - 1)}
+                  disabled={pagination.current_page === 1}
+                  className="pagination-btn"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" /></svg>
+                  Previous
+                </button>
+                <span className="pagination-text">
+                  Page {pagination.current_page} of {pagination.last_page}
+                </span>
+                <button
+                  onClick={() => getReport(pagination.current_page + 1)}
+                  disabled={pagination.current_page === pagination.last_page}
+                  className="pagination-btn"
+                >
+                  Next
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
+                </button>
               </div>
             )}
+            {/* --- END: UPDATED PAGINATION CONTROLS --- */}
           </>
         ) : (
           <p className="text-center">No report data available for this period.</p>
