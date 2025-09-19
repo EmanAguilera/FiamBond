ec4fcc6fe4b119f1803f46fc18f54ab4ee857674
Eman <emanaguilera711@gmail.com>
2025-09-19 02:51:35 -0700

    Design: Delete button for goal

--- my_cointrak_react/src/Pages/Goals/Index.jsx
+++ my_cointrak_react/src/Pages/Goals/Index.jsx
@@ -250,7 +250,7 @@ export default function Goals() {
                 <div className="text-right mt-3 space-x-2">
                   <button onClick={() => handleMarkAsComplete(goal.id)} className="secondary-btn text-xs">Mark as Complete</button>
                   {/* I recommend adding a more distinct style for a destructive action */}
-                  <button onClick={() => handleDeleteGoal(goal.id)} className="danger-btn text-xs">Abandon</button>
+                  <button onClick={() => handleDeleteGoal(goal.id)} className="danger-btn-sm text-xs">Abandon</button>
                 </div>
                 {/* --- END OF DELETION FEATURE --- */}
               </div>
